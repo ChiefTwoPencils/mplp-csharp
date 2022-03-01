0c58afd37aeea4e886ce4f8a90135c9df293e62d
ChiefTwoPencils <robertwilk916@gmail.com>
2022-02-28 21:17:09 -0800

    Draws org charts

---
+++ nary_node6/NaryNode.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace nary_node6
+{
+    public class NaryNode<T>
+    {
+        #region Ctors, Consts, and Props
+
+        private const double Width = 80;
+        private const double Height = 40;
+        private const double Indent = 20;
+        private const double XSpacing = 40.00;
+        private const double YSpacing = 20.00;
+
+        private double HalfWidth { get; set; } = Width / 2;
+        private double HalfHeight { get; set; } = Height / 2;
+        public Point Center { get; private set; }
+        public Rect SubtreeBounds { get; private set; }
+        public T Value { get; set; }
+        public List<NaryNode<T>> Children { get; }
+
+        public NaryNode(T value)
+        {
+            Value = value;
+            Children = new List<NaryNode<T>>();
+        }
+
+        #endregion
+
+        #region Node Ops
+
+        public NaryNode<T> AddChild(NaryNode<T> child)
+        {
+            if (child == null)
+            {
+                throw new ArgumentNullException($"{nameof(child)} must not be null");
+            }
+
+            Children.Add(child);
+            return this;
+        }
+
+        public NaryNode<T> AddChildren(params NaryNode<T>[] children)
+        {
+            if (children == null || children.Any(child => child == null))
+            {
+                throw new ArgumentException($"{nameof(children)} and its elements must not be null");
+            }
+
+            Children.AddRange(children);
+            return this;
+        }
+
+        public NaryNode<T> AddChildren(params T[] values)
+            => AddChildren(values.Select(v => new NaryNode<T>(v)).ToArray());
+
+        public NaryNode<T> FindNode(T value) => FindNode(this, value);
+
+        public NaryNode<T> FindNode(NaryNode<T> root, T value)
+        {
+            if (root == null || Equals(root.Value, value)) return root;
+
+            foreach (var child in root.Children)
+            {
+                if (child.FindNode(value) != null) return child;
+            }
+
+            return null;
+        }
+
+        public bool IsLeaf() => HasEmptyNest();
+
+        public bool IsTwig() => Children.All(child => child.IsLeaf());
+
+        #endregion
+
+        #region Traversals
+
+        public List<NaryNode<T>> TraversePreOrder() => TraversePreOrder(this);
+
+        private List<NaryNode<T>> TraversePreOrder(NaryNode<T> root)
+        {
+            if (root == null) return new List<NaryNode<T>>();
+
+            var list = new List<NaryNode<T>> { root };
+            list.AddRange(root.Children.SelectMany(c => TraversePreOrder(c)));
+            return list;
+        }
+
+        public List<NaryNode<T>> TraversePostOrder() => TraversePostOrder(this);
+
+        private List<NaryNode<T>> TraversePostOrder(NaryNode<T> root)
+        {
+            if (root == null) return new List<NaryNode<T>>();
+
+            var list = root.Children
+                .SelectMany(c => TraversePostOrder(c))
+                .ToList();
+            list.Add(root);
+            return list;
+        }
+
+        public Queue<NaryNode<T>> TraverseBreadthFirst()
+        {
+            var queue = new Queue<NaryNode<T>>();
+            queue.Enqueue(this);
+            return TraverseBreadthFirst(queue, this.Children);
+        }
+
+        private Queue<NaryNode<T>> TraverseBreadthFirst(Queue<NaryNode<T>> queue, List<NaryNode<T>> children)
+        {
+            if (children == null || !children.Any()) return queue;
+
+            children.ForEach(c => queue.Enqueue(c));
+            TraverseBreadthFirst(queue, children.SelectMany(c => c.Children).ToList());
+
+            return queue;
+        }
+
+        #endregion
+         
+        #region Drawing
+
+        public void ArrangeAndDrawSubtree(Canvas canvas, double minX, double minY)
+        {
+            ArrangeSubtree(minX, minY);
+            DrawSubtreeLinks(canvas);
+            DrawSubtreeNodes(canvas);
+        }
+
+        private void ArrangeSubtree(double minX, double minY)
+        {
+            if (IsLeaf())
+            {
+                SubtreeBounds = new Rect(minX, minY, Width, Height);
+                Center = new Point(minX + HalfWidth, minY + HalfHeight);
+                return;
+            }
+
+            var baseY = minY + Height + YSpacing;
+
+            if (IsTwig())
+            {                
+                var x = minX + Indent;
+                var y = baseY;
+
+                var count = Children.Count;
+                for (var i = 0; i < count; i++)
+                {
+                    var child = Children[i];
+                    child.ArrangeSubtree(x, y);
+                    y = child.SubtreeBounds.Bottom + YSpacing;
+                }
+
+                SubtreeBounds = new Rect(minX, minY, Width + Indent, y - YSpacing - minY);
+                Center = new Point(minX + HalfWidth, minY + HalfHeight);
+            }
+            else
+            {
+                var totalChildWidth = 0.0;
+                var count = Children.Count;
+                var height = 0.0;
+
+                for (var i = 0; i < count; ++i)
+                {
+                    var child = Children[i];
+                    var childMinX = minX + totalChildWidth + i * XSpacing;
+                    child.ArrangeSubtree(childMinX, baseY);
+                    totalChildWidth += child.SubtreeBounds.Width;
+                    height = Math.Max(height, child.SubtreeBounds.Height);
+                }
+
+                SubtreeBounds = new Rect(minX, minY, totalChildWidth + (count - 1) * XSpacing, height);
+                Center = new Point(minX + SubtreeBounds.Width / 2, minY + HalfHeight);
+            }
+        }
+
+        private void DrawSubtreeNodes(Canvas canvas)
+        {
+            TraverseBreadthFirst()
+                .ToList()
+                .ForEach(n =>
+                {
+                    #region Debug Node Bounds
+                    //var subBox = canvas.DrawRectangle(
+                    //    n.SubtreeBounds, Brushes.Transparent, Brushes.Red, 1);
+                    #endregion
+
+                    var fill = n.HasEmptyNest() ? Brushes.White : Brushes.LightPink;
+                    var node = canvas.DrawRectangle(
+                        new Rect(n.Center.X - HalfWidth, n.Center.Y - HalfHeight, Width, Height), fill, Brushes.Black, 1);
+
+                    string value = n.Value.ToString();
+                    value = value.Length > 10
+                        ? string.Join("\n", value.Split(' '))
+                        : value;
+
+                    var label = canvas.DrawLabel(
+                        new Rect(
+                            n.Center.X - HalfWidth,
+                            n.Center.Y - HalfHeight,
+                            Width,
+                            Height),
+                        value,
+                        Brushes.Transparent,
+                        Brushes.Red,
+                        HorizontalAlignment.Center,
+                        VerticalAlignment.Center,
+                        HalfHeight / 2,
+                        1);
+                });
+        }
+
+        private void DrawSubtreeLinks(Canvas canvas)
+        {
+            void Connector(Point start, Point end) => canvas.DrawLine(start, end, Brushes.Green, 1);
+
+            TraverseBreadthFirst()
+                .ToList()
+                .ForEach(n =>
+                {
+                    if (n.IsLeaf())
+                    {
+                        return;
+                    }
+
+                    if (n.IsTwig())
+                    {
+                        var lastPoint = new Point(0, 0);
+                        n.Children.ForEach(child =>
+                        {
+                            var x = child.SubtreeBounds.Left;
+                            var y = child.SubtreeBounds.Top + HalfHeight;
+                            lastPoint = new Point(x - Indent / 2, y);
+                            Connector(new Point(x, y), lastPoint);
+                        });
+                         
+                        var startX = n.SubtreeBounds.Left + Indent / 2;
+                        var startY = n.SubtreeBounds.Top + Height;
+                        Connector(new Point(startX, startY), lastPoint);
+                    }
+                    else
+                    {
+                        var center = n.Center;
+                        var halfWay = HalfHeight + YSpacing / 2;
+                        var startX = double.MaxValue;
+                        var endX = double.MinValue;
+                        var startEndY = 0.0;                        
+
+                        n.Children.ForEach(child =>
+                        {
+                            var childCenter = child.Center;
+                            startX = Math.Min(startX, childCenter.X);
+                            endX = Math.Max(endX, childCenter.X);
+                            startEndY = childCenter.Y - halfWay;
+                            Connector(childCenter, new Point(childCenter.X, childCenter.Y - halfWay));
+                        });
+
+                        Connector(center, new Point(center.X, center.Y + halfWay));
+                        Connector(new Point(startX, startEndY), new Point(endX, startEndY));
+                    }                 
+                });
+        }
+
+        private bool HasEmptyNest() => !Children.Any();
+
+        private bool HasOneChild() => Children.Count == 1;
+
+        #endregion
+
+        #region Helpers
+
+        public override string ToString() => ToString(string.Empty);
+
+        public string ToString(string spaces)
+        {
+            var thisValue = $"{spaces}{Value}:";
+            if (!Children.Any())
+            {
+                return thisValue;
+            }
+
+            var indentation = $"{spaces}  ";
+            var childrenValues = Children
+                .Select(child => child.ToString(indentation));
+
+            return $"{thisValue}\n{string.Join("\n", childrenValues)}";
+        }
+
+        #endregion
+    }
+}
+    
---
+++ nary_node6/Window1.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nary_node6
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            Height = 500;
+            Width = 750;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Build a test tree.
+            // A
+            //         |
+            //     +---+---+
+            // B   C   D
+            //     |       |
+            //    +-+      +
+            // E F      G
+            //    |        |
+            //    +      +-+-+
+            // H      I J K
+            NaryNode<string> node_a = new NaryNode<string>("A");
+            NaryNode<string> node_b = new NaryNode<string>("B");
+            NaryNode<string> node_c = new NaryNode<string>("C");
+            NaryNode<string> node_d = new NaryNode<string>("D");
+            NaryNode<string> node_e = new NaryNode<string>("E");
+            NaryNode<string> node_f = new NaryNode<string>("F");
+            NaryNode<string> node_g = new NaryNode<string>("G");
+            NaryNode<string> node_h = new NaryNode<string>("H");
+            NaryNode<string> node_i = new NaryNode<string>("I");
+            NaryNode<string> node_j = new NaryNode<string>("J");
+            NaryNode<string> node_k = new NaryNode<string>("K");
+
+            node_a.AddChild(node_b);
+            //node_a.AddChild(node_c);
+            node_a.AddChild(node_d);
+            node_b.AddChild(node_e);
+            node_b.AddChild(node_f);
+            node_d.AddChild(node_g);
+            node_e.AddChild(node_h);
+            node_g.AddChild(node_i);
+            node_g.AddChild(node_j);
+            node_g.AddChild(node_k);            
+
+            var rd = new NaryNode<string>("R & D");
+            rd.AddChildren(new string[] { "Applied", "Basic", "Advanced", "Sci Fi" });
+
+            var sales = new NaryNode<string>("Sales");
+            sales.AddChildren(new string[] { "Inside Sales", "Outside Sales", "B2B", "Consumer", "Account Management" });
+
+            var hr = new NaryNode<string>("HR");
+            hr.AddChildren(new string[] { "Training", "Hiring", "Equity", "Discipline" });
+
+            var accounting = new NaryNode<string>("Accounting");
+            accounting.AddChildren(new string[] { "Payroll", "Billing", "Reporting", "Opacity" });
+
+            var legal = new NaryNode<string>("Legal");
+            legal.AddChildren(new string[] { "Compliance", "Progress Prevention", "Bail Services" });
+
+            var profSrvs = new NaryNode<string>("Professional Services")
+                .AddChildren(hr, accounting, legal);
+
+            var root = new NaryNode<string>("GeneriGloop")
+                .AddChildren(rd, sales, profSrvs);
+
+            // Draw the tree.
+            root.ArrangeAndDrawSubtree(mainCanvas, 10, 10);
+        }
+    }
+}
