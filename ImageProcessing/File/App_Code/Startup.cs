43660560d1a0d56faa477e8ba729401d4f5b6f1f
ChiefTwoPencils <robertwilk916@gmail.com>
2022-03-15 11:23:31 -0700

    Has ImageProcessing sln and File proj

---
+++ ImageProcessing/File/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(File.Startup))]
+namespace File
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
